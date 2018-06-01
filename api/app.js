122b46519943da0ff79c0565d6e8f3535694f89a
juaning <juaning@gmail.com>
2018-05-31 20:24:01 -0400

    Update app with new routes, models and error handling for Mongo

--- api/app.js
+++ api/app.js
@@ -1,16 +1,70 @@
+require('./config/config');
+require('./global_functions');
+
 const express = require('express');
 const morgan = require('morgan');
-const router = require('./src/routes');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const v1 = require('./src/routes/v1');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Please go to /api');
+// Passport
+app.use(passport.initialize());
+
+// Models
+const models = require('./src/models');
+
+// CORS. So other sites can access our API
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, content-type, Authorization, Content-Type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
 
-app.use('/api', router);
+app.use('/v1', v1);
+
+app.use('/', (req, res) => {
+  res.statusCode = 200;
+  res.json({
+    status: 'success',
+    message: 'Parcel Pending API',
+    data: {},
+  });
+});
+
+// Catch 404 & forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  // Only provide error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('emu API listening on 3000...'));
+
+module.exports = app;
