b408fbd29863e09393ddb280e4dae0cdaeb4ddea
juaning <juaning@gmail.com>
2018-06-01 09:10:42 -0400

    Fix linting errors

--- api/app.js
+++ api/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-Requested-With, content-type, Authorization, Content-Type'
+    'X-Requested-With, content-type, Authorization, Content-Type',
   );
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
--- api/src/models/index.js
+++ api/src/models/index.js
@@ -7,25 +7,27 @@ const basename = path.basename(__filename);
 const models = {};
 
 if (CONFIG.db_host !== '') {
+  // eslint-disable-next-line no-unused-vars
   const files = fs
     .readdirSync(__dirname)
-    .filter((file) => {
-      return (
-        file.IndexOf('.') !== 0)
-        && (file !== basename)
-        && (file.slice(-3) === '.js')
-      );
-    })
+    .filter(file => (
+      (file.IndexOf('.') !== 0)
+      && (file !== basename)
+      && (file.slice(-3) === '.js')
+    ))
     .forEach((file) => {
       const filename = file.split('.')[0];
-      const model_name = filename.chartAt(0).toUpperCase() + filename.slice(1);
+      const modelName = filename.chartAt(0).toUpperCase() + filename.slice(1);
+      /* eslint-disable import/no-dynamic-require */
+      // eslint-disable-next-line global-require
+      models[modelName] = require(`./${file}`);
+      /* eslint-enable import/no-dynamic-require */
     });
-  
   mongoose.Promise = global.Promise;
   const mongoLocation = process.env.MONGO_URL;
-
+  /* eslint-disable no-console */
   mongoose.connect(mongoLocation)
-    .catch(err => console
+    .catch(() => console
       .log(`*** Can not connect to Mongo Server: ${mongoLocation}`));
 
   const db = mongoose.connection;
@@ -36,3 +38,4 @@ if (CONFIG.db_host !== '') {
   console.log('No mongo credentials given');
 }
 
+/* eslint-enable no-console */
