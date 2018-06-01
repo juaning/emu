89617fa03970409e140e07407ba804e6d4c0f355
juaning <juaning@gmail.com>
2018-06-01 17:56:30 -0400

    Add PersonalData model with mongoose

---
+++ api/src/models/PersonalData.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validate = require('mongoose-validator');
+
+const PersonalDataSchema = mongoose.Schema({
+  documentId: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  DOB: { type: Date, required: true },
+  maritalStatus: {
+    type: String,
+    enum: ['single', 'married', 'divorced', 'other'],
+  },
+  address: { type: String },
+  phone: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    validate: [
+      validate({
+        validator: 'isNumeric',
+        arguments: [6, 20],
+        message: 'Not a valid phone number.',
+      }),
+    ],
+  },
+  nationality: { type: String },
+  gender: { type: String, enum: ['none', 'female', 'male'] },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    index: true,
+    unique: true,
+    validate: [
+      validate({
+        validator: 'isEmail',
+        message: 'Not a valid email.',
+      }),
+    ],
+  },
+  updated: { type: Date, default: Date.now },
+}, { timestamps: true });
+
+PersonalDataSchema.methods.toWeb = () => {
+  const json = this.toJSON();
+  // eslint-disable-next-line no-underscore-dangle
+  json.id = this._id;
+  return json;
+};
+
+module.exports = mongoose.model('PersonalData', PersonalDataSchema);
