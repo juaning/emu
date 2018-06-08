7a0c52b397117edf707a432dfa3a8f6df1a2977d
juaning <juaning@gmail.com>
2018-06-08 11:33:16 -0400

    Add mongo and delete routes for Postgres

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,15 +1,18 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
 import Routes from './routes';
 
 class App {
   public app: express.Application;
   public routePrv: Routes = new Routes();
+  public mongoUrl: string = process.env.MONGO_URL;
 
   constructor() {
     this.app = express();
     this.config();
     this.routePrv.routes(this.app);
+    this.mongoSetup();
   }
 
   private config(): void {
@@ -18,6 +21,11 @@ class App {
 
     // support application/x-www-form-urlencoded post data
     this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  private mongoSetup(): void {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(this.mongoUrl);
   }
 }
 
