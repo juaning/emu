71b3c13b276b90068acb2cd215c4a8d335a36846
juaning <juaning@gmail.com>
2018-08-23 10:50:52 -0400

    Update Wizard view

--- webapp/src/views/pro/Forms/Wizard.jsx
+++ webapp/src/views/pro/Forms/Wizard.jsx
@@ -1,33 +1,29 @@
-import React from "react";
+import React from 'react';
 
 // core components
-import Wizard from "components/Wizard/Wizard.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
+import Wizard from '../../../components/Wizard/Wizard';
+import GridContainer from '../../../components/Grid/GridContainer';
+import GridItem from '../../../components/Grid/GridItem';
 
-import Step1 from "./WizardSteps/Step1.jsx";
-import Step2 from "./WizardSteps/Step2.jsx";
-import Step3 from "./WizardSteps/Step3.jsx";
+import Step1 from './WizardSteps/Step1';
+import Step2 from './WizardSteps/Step2';
+import Step3 from './WizardSteps/Step3';
 
-class WizardView extends React.Component {
-  render() {
-    return (
-      <GridContainer justify="center">
-        <GridItem xs={12} sm={8}>
-          <Wizard
-            validate
-            steps={[
-              { stepName: "About", stepComponent: Step1, stepId: "about" },
-              { stepName: "Account", stepComponent: Step2, stepId: "account" },
-              { stepName: "Address", stepComponent: Step3, stepId: "address" }
-            ]}
-            title="Build Your Profile"
-            subtitle="This information will let us know more about you."
-          />
-        </GridItem>
-      </GridContainer>
-    );
-  }
-}
+const WizardView = () => (
+  <GridContainer justify="center">
+    <GridItem xs={12} sm={8}>
+      <Wizard
+        validate
+        steps={[
+          { stepName: 'About', stepComponent: Step1, stepId: 'about' },
+          { stepName: 'Account', stepComponent: Step2, stepId: 'account' },
+          { stepName: 'Address', stepComponent: Step3, stepId: 'address' },
+        ]}
+        title="Build Your Profile"
+        subtitle="This information will let us know more about you."
+      />
+    </GridItem>
+  </GridContainer>
+);
 
 export default WizardView;
