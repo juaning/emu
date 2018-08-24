4057836b7cff4913f16e1611444927c4ce527e87
juaning <juaning@gmail.com>
2018-08-24 16:36:38 -0400

    Update HeadeLinks to fix issues

--- webapp/src/components/Header/HeaderLinks.jsx
+++ webapp/src/components/Header/HeaderLinks.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import { Manager, Target, Popper } from 'react-popper';
+import { Manager, Reference, Popper } from 'react-popper';
 
 // @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -97,88 +97,106 @@ class HeaderLinks extends React.Component {
           </Hidden>
         </Button>
         <Manager className={managerClasses}>
-          <Target>
-            <Button
-              color="transparent"
-              justIcon
-              aria-label="Notifications"
-              aria-owns={open ? 'menu-list' : null}
-              aria-haspopup="true"
-              onClick={this.handleClick}
-              className={rtlActive ? classes.buttonLinkRTL : classes.buttonLink}
-              muiClasses={{
-                label: rtlActive ? classes.labelRTL : '',
-              }}
-            >
-              <Notifications
-                className={
-                  `${classes.headerLinksSvg} ${rtlActive
-                    ? `${classes.links} ${classes.linksRTL}`
-                    : classes.links}`
-                }
-              />
-              <span className={classes.notifications}>5</span>
-              <Hidden mdUp>
-                <button onClick={this.handleClick} className={classes.linkText}>
-                  {rtlActive ? 'إعلام' : 'Notification'}
-                </button>
-              </Hidden>
-            </Button>
-          </Target>
+          <Reference>
+            {({ ref }) => (
+              <Button
+                ref={ref}
+                color="transparent"
+                justIcon
+                aria-label="Notifications"
+                aria-owns={open ? 'menu-list' : null}
+                aria-haspopup="true"
+                onClick={this.handleClick}
+                className={rtlActive ? classes.buttonLinkRTL : classes.buttonLink}
+                muiClasses={{
+                  label: rtlActive ? classes.labelRTL : '',
+                }}
+              >
+                <Notifications
+                  className={
+                    `${classes.headerLinksSvg} ${rtlActive
+                      ? `${classes.links} ${classes.linksRTL}`
+                      : classes.links}`
+                  }
+                />
+                <span className={classes.notifications}>5</span>
+                <Hidden mdUp>
+                  <button onClick={this.handleClick} className={classes.linkText}>
+                    {rtlActive ? 'إعلام' : 'Notification'}
+                  </button>
+                </Hidden>
+              </Button>
+            )}
+          </Reference>
           <Popper
             placement="bottom-start"
             eventsEnabled={open}
             className={
               `${classNames({ [classes.popperClose]: !open })} ${classes.pooperResponsive}`}
           >
-            <ClickAwayListener onClickAway={this.handleClose}>
-              <Grow
-                in={open}
-                id="menu-list"
-                style={{ transformOrigin: '0 0 0' }}
+            {({ ref, placement }) => (
+              <div
+                ref={ref}
+                className={classNames(
+                  { [classes.popperClose]: !open },
+                  { [classes.pooperResponsive]: true },
+                )}
+                style={{
+                  position: 'absolute',
+                  willChange: 'transform',
+                }}
+                data-placement={placement}
               >
-                <Paper className={classes.dropdown}>
-                  <MenuList role="menu">
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={dropdownItem}
-                    >
-                      {rtlActive
-                        ? 'إجلاء أوزار الأسيوي حين بل, كما'
-                        : 'Mike John responded to your email'}
-                    </MenuItem>
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={dropdownItem}
-                    >
-                      {rtlActive
-                        ? 'شعار إعلان الأرضية قد ذلك'
-                        : 'You have 5 new tasks'}
-                    </MenuItem>
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={dropdownItem}
-                    >
-                      {rtlActive
-                        ? 'ثمّة الخاصّة و على. مع جيما'
-                        : "You're now friend with Andrew"}
-                    </MenuItem>
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={dropdownItem}
-                    >
-                      {rtlActive ? 'قد علاقة' : 'Another Notification'}
-                    </MenuItem>
-                    <MenuItem
-                      onClick={this.handleClose}
-                      className={dropdownItem}
-                    >
-                      {rtlActive ? 'قد فاتّبع' : 'Another One'}
-                    </MenuItem>
-                  </MenuList>
-                </Paper>
-              </Grow>
-            </ClickAwayListener>
+                <ClickAwayListener onClickAway={this.handleClose}>
+                  <Grow
+                    in={open}
+                    id="menu-list"
+                    style={{ transformOrigin: '0 0 0' }}
+                  >
+                    <Paper className={classes.dropdown}>
+                      <MenuList role="menu">
+                        <MenuItem
+                          onClick={this.handleClose}
+                          className={dropdownItem}
+                        >
+                          {rtlActive
+                            ? 'إجلاء أوزار الأسيوي حين بل, كما'
+                            : 'Mike John responded to your email'}
+                        </MenuItem>
+                        <MenuItem
+                          onClick={this.handleClose}
+                          className={dropdownItem}
+                        >
+                          {rtlActive
+                            ? 'شعار إعلان الأرضية قد ذلك'
+                            : 'You have 5 new tasks'}
+                        </MenuItem>
+                        <MenuItem
+                          onClick={this.handleClose}
+                          className={dropdownItem}
+                        >
+                          {rtlActive
+                            ? 'ثمّة الخاصّة و على. مع جيما'
+                            : "You're now friend with Andrew"}
+                        </MenuItem>
+                        <MenuItem
+                          onClick={this.handleClose}
+                          className={dropdownItem}
+                        >
+                          {rtlActive ? 'قد علاقة' : 'Another Notification'}
+                        </MenuItem>
+                        <MenuItem
+                          onClick={this.handleClose}
+                          className={dropdownItem}
+                        >
+                          {rtlActive ? 'قد فاتّبع' : 'Another One'}
+                        </MenuItem>
+                      </MenuList>
+                    </Paper>
+                  </Grow>
+                </ClickAwayListener>
+              </div>
+            )}
           </Popper>
         </Manager>
         <Button
