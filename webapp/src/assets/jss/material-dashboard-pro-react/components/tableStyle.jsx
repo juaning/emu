1676e055e730cb1b6381637f79af16567c23ab3e
juaning <juaning@gmail.com>
2018-08-28 10:31:27 -0400

    Add missing images and update path for table styles

--- webapp/src/assets/jss/material-dashboard-pro-react/components/tableStyle.jsx
+++ webapp/src/assets/jss/material-dashboard-pro-react/components/tableStyle.jsx
@@ -10,117 +10,117 @@ import {
   infoColor,
   roseColor,
   grayColor,
-  defaultFont
-} from "assets/jss/material-dashboard-pro-react.jsx";
+  defaultFont,
+} from '../../material-dashboard-pro-react';
 
 const tableStyle = theme => ({
   warning: {
-    color: warningColor
+    color: warningColor,
   },
   primary: {
-    color: primaryColor
+    color: primaryColor,
   },
   danger: {
-    color: dangerColor
+    color: dangerColor,
   },
   success: {
-    color: successColor
+    color: successColor,
   },
   info: {
-    color: infoColor
+    color: infoColor,
   },
   rose: {
-    color: roseColor
+    color: roseColor,
   },
   gray: {
-    color: grayColor
+    color: grayColor,
   },
   right: {
-    textAlign: "right"
+    textAlign: 'right',
   },
   table: {
-    marginBottom: "0",
-    width: "100%",
-    maxWidth: "100%",
-    backgroundColor: "transparent",
-    borderSpacing: "0",
-    borderCollapse: "collapse",
-    overflow: "auto"
+    marginBottom: '0',
+    width: '100%',
+    maxWidth: '100%',
+    backgroundColor: 'transparent',
+    borderSpacing: '0',
+    borderCollapse: 'collapse',
+    overflow: 'auto',
   },
   tableShoppingHead: {
-    fontSize: "0.9em !important",
-    textTransform: "uppercase !important"
+    fontSize: '0.9em !important',
+    textTransform: 'uppercase !important',
   },
   tableHeadFontSize: {
-    fontSize: "1.25em !important"
+    fontSize: '1.25em !important',
   },
   tableHeadCell: {
-    color: "rgba(0, 0, 0, 0.87)",
-    border: "none !important"
+    color: 'rgba(0, 0, 0, 0.87)',
+    border: 'none !important',
   },
   tableCell: {
     ...defaultFont,
-    lineHeight: "1.42857143",
-    padding: "12px 8px!important",
-    verticalAlign: "middle",
-    fontSize: "1em",
-    borderBottom: "none",
-    borderTop: "1px solid #ddd",
-    position: "relative",
-    [theme.breakpoints.down("sm")]: {
-      minHeight: "24px",
-      minWidth: "32px",
-    }
+    lineHeight: '1.42857143',
+    padding: '12px 8px!important',
+    verticalAlign: 'middle',
+    fontSize: '1em',
+    borderBottom: 'none',
+    borderTop: '1px solid #ddd',
+    position: 'relative',
+    [theme.breakpoints.down('sm')]: {
+      minHeight: '24px',
+      minWidth: '32px',
+    },
   },
   tableCellTotal: {
-    fontWeight: "500",
-    fontSize: "1.25em",
-    paddingTop: "14px",
-    textAlign: "right"
+    fontWeight: '500',
+    fontSize: '1.25em',
+    paddingTop: '14px',
+    textAlign: 'right',
   },
   tableCellAmount: {
-    fontSize: "26px",
-    fontWeight: "300",
-    marginTop: "5px",
-    textAlign: "right"
+    fontSize: '26px',
+    fontWeight: '300',
+    marginTop: '5px',
+    textAlign: 'right',
   },
   tableResponsive: {
     // width: "100%",
-    minHeight: "0.1%",
-    overflowX: "auto"
+    minHeight: '0.1%',
+    overflowX: 'auto',
   },
   tableStripedRow: {
-    backgroundColor: "#f9f9f9"
+    backgroundColor: '#f9f9f9',
   },
   tableRowHover: {
-    "&:hover": {
-      backgroundColor: "#f5f5f5"
-    }
+    '&:hover': {
+      backgroundColor: '#f5f5f5',
+    },
   },
   warningRow: {
-    backgroundColor: "#fcf8e3",
-    "&:hover": {
-      backgroundColor: "#faf2cc"
-    }
+    backgroundColor: '#fcf8e3',
+    '&:hover': {
+      backgroundColor: '#faf2cc',
+    },
   },
   dangerRow: {
-    backgroundColor: "#f2dede",
-    "&:hover": {
-      backgroundColor: "#ebcccc"
-    }
+    backgroundColor: '#f2dede',
+    '&:hover': {
+      backgroundColor: '#ebcccc',
+    },
   },
   successRow: {
-    backgroundColor: "#dff0d8",
-    "&:hover": {
-      backgroundColor: "#d0e9c6"
-    }
+    backgroundColor: '#dff0d8',
+    '&:hover': {
+      backgroundColor: '#d0e9c6',
+    },
   },
   infoRow: {
-    backgroundColor: "#d9edf7",
-    "&:hover": {
-      backgroundColor: "#c4e3f3"
-    }
-  }
+    backgroundColor: '#d9edf7',
+    '&:hover': {
+      backgroundColor: '#c4e3f3',
+    },
+  },
 });
 
 export default tableStyle;
